aebe29c04478f14623183ddf703d66a6ff7218a8
ah23007CesiaAlfaro <ah23007@ues.edu.sv>
2026-05-31 16:51:13 -0600

    model/prestamo.py

---
+++ app/models/prestamo.py
@@ -0,0 +1,14 @@
+class prestamo:
+    def __init(self,id_prestamo,id_socio, id_libro):
+        self._id_prestamo=id_prestamo
+        self._id_socio=id_socio
+        self._id_libro=id_libro
+
+    def get_id(self):
+        return self._id_prestamo
+    
+
+    def get_id_socio(self):
+        return self._id_socio
+    def get_id_libro(self):
+        return self._id_libro
